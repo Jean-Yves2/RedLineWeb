6ea2205a4a328feecd1dd2cdcbe41af703505275
Jean-Yves2 <sarakajean1@gmail.com>
2024-07-02 09:48:48 +0200

    feat : Test token on local storage

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -9,8 +9,15 @@ import { environment } from '../../../environments/environment';
 })
 export class AuthService {
   private apiUrl = environment.apiUrl;
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(
+      JSON.parse(localStorage.getItem('currentUser')!)
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   register(registerForm: any): Observable<any> {
     const headers = new HttpHeaders({
@@ -19,5 +26,25 @@ export class AuthService {
     return this.http
       .post(`${this.apiUrl}/auth/register`, registerForm, { headers })
       .pipe(catchError((error) => throwError(() => error)));
+  }
+
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
+  login(email: string, password: string) {
+    return this.http
+      .post<any>(`${this.apiUrl}/auth/login`, { email, password })
+      .pipe(
+        map((response) => {
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          this.currentUserSubject.next(response);
+          return response;
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
 }
