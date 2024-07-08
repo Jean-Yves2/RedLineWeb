a7a4e2f7e0c7ef0ac4fe0a74426c160a07ce08e2
Jean-Yves2 <sarakajean1@gmail.com>
2024-07-08 12:56:01 +0200

    feat : logout router in toolbar

--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import Cookies from 'js-cookie';
 
 @Component({
   selector: 'app-tool-bar',
   templateUrl: './tool-bar.component.html',
   styleUrls: ['./tool-bar.component.scss'],
 })
-export class ToolBarComponent {}
+export class ToolBarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/connexion']);
+  }
+}
