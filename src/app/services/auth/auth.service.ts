f818a153da2a56929e1eb742d0d464fb1e957600
Jean-Yves2 <sarakajean1@gmail.com>
2024-07-08 12:56:44 +0200

    fix : logout

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import Cookies from 'js-cookie';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +14,7 @@ export class AuthService {
   private currentUserSubject: BehaviorSubject<any>;
   public currentUser: Observable<any>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.currentUserSubject = new BehaviorSubject<any>(
       JSON.parse(localStorage.getItem('currentUser')!)
     );
@@ -31,20 +33,37 @@ export class AuthService {
   public get currentUserValue(): any {
     return this.currentUserSubject.value;
   }
-  login(email: string, password: string) {
-    return this.http
-      .post<any>(`${this.apiUrl}/auth/login`, { email, password })
-      .pipe(
-        map((response) => {
-          localStorage.setItem('currentUser', JSON.stringify(response));
-          this.currentUserSubject.next(response);
-          return response;
-        })
-      );
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(
+      `${this.apiUrl}/auth/login`,
+      { email, password },
+      { withCredentials: true }
+    );
   }
 
-  logout() {
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
+  setSession(authResult: any) {
+    Cookies.set('access_token', authResult.access_token);
+  }
+
+  logout(): void {
+    this.http
+      .post(`${this.apiUrl}/auth/logout`, {}, { withCredentials: true })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/connexion']);
+        },
+        error: (error) => {
+          console.error('Erreur lors de la déconnexion côté serveur : ', error);
+        },
+      });
+  }
+
+  public isLoggedIn(): boolean {
+    return !!Cookies.get('access_token');
+  }
+
+  public getToken(): string | undefined {
+    return Cookies.get('access_token');
   }
 }
