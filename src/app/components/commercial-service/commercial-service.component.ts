844bad69ebdd870d1a762748f034db1b4323a58d
Jean-Yves2 <sarakajean1@gmail.com>
2024-08-01 00:32:51 +0200

    feat : get all users

--- src/app/components/commercial-service/commercial-service.component.ts
+++ src/app/components/commercial-service/commercial-service.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user/user-service.service';
 
 @Component({
   selector: 'app-commercial-service',
   templateUrl: './commercial-service.component.html',
   styleUrls: ['./commercial-service.component.scss'],
 })
-export class CommercialServiceComponent {
+export class CommercialServiceComponent implements OnInit {
   currentUser: any;
   currentUserName: string | undefined;
   firstLetter: string | undefined;
   isRotated = false;
+  users: any[] = [];
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) {
     this.authService.currentUser.subscribe((user) => {
       this.currentUser = user;
       this.currentUserName = user.firstName;
       this.getFirstLetter();
     });
+  }
+
+  ngOnInit() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    this.userService.getUsers().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des utilisateurs', error);
+      }
+    );
   }
 
   logout() {
