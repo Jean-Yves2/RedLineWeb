e8f7d1e72259c6d7541c543cddbf87dad00f454a
Jean-Yves2 <sarakajean1@gmail.com>
2024-07-31 16:05:59 +0200

    feat : Add menu sections to commercial-service component

--- src/app/components/commercial-service/commercial-service.component.ts
+++ src/app/components/commercial-service/commercial-service.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-commercial-service',
   templateUrl: './commercial-service.component.html',
-  styleUrls: ['./commercial-service.component.scss']
+  styleUrls: ['./commercial-service.component.scss'],
 })
 export class CommercialServiceComponent {
+  currentUser: any;
+  currentUserName: string | undefined;
+  fistLetter: string | undefined;
+  isRotated = false;
 
+  constructor(private authService: AuthService, private router: Router) {
+    this.authService.currentUser.subscribe((user) => {
+      this.currentUser = user;
+      this.currentUserName = user.firstName;
+      this.getFirstLetter();
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/connexion']);
+  }
+
+  getFirstLetter() {
+    if (this.currentUserName) {
+      this.fistLetter = this.currentUserName.charAt(0);
+    }
+  }
+
+  rotateElement() {
+    this.isRotated = !this.isRotated;
+  }
 }
