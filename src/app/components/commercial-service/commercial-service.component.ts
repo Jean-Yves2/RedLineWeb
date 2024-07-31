57b408396c4ce51baad23ee39efa31647168755c
Jean-Yves2 <sarakajean1@gmail.com>
2024-08-01 00:52:43 +0200

    feat : add  sort by user name

--- src/app/components/commercial-service/commercial-service.component.ts
+++ src/app/components/commercial-service/commercial-service.component.ts
@@ -14,6 +14,11 @@ export class CommercialServiceComponent implements OnInit {
   firstLetter: string | undefined;
   isRotated = false;
   users: any[] = [];
+  paginatedUsers: any[] = [];
+  currentPage = 1;
+  totalPages = 1;
+  pageSize = 12;
+  sortDirection: 'asc' | 'desc' = 'asc';
 
   constructor(
     private authService: AuthService,
@@ -35,6 +40,9 @@ export class CommercialServiceComponent implements OnInit {
     this.userService.getUsers().subscribe(
       (data) => {
         this.users = data;
+        this.totalPages = Math.ceil(this.users.length / this.pageSize);
+        this.setPage(this.currentPage);
+        console.log(this.users);
       },
       (error) => {
         console.error('Erreur lors de la récupération des utilisateurs', error);
@@ -55,5 +63,39 @@ export class CommercialServiceComponent implements OnInit {
 
   rotateElement() {
     this.isRotated = !this.isRotated;
+  }
+
+  sortUsers() {
+    this.users.sort((a, b) => {
+      const nameA = a.firstName.toLowerCase();
+      const nameB = b.firstName.toLowerCase();
+      if (nameA < nameB) {
+        return this.sortDirection === 'asc' ? -1 : 1;
+      }
+      if (nameA > nameB) {
+        return this.sortDirection === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+    this.setPage(this.currentPage);
+  }
+
+  toggleSortDirection() {
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    this.sortUsers();
+  }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.totalPages) {
+      return;
+    }
+    this.currentPage = page;
+    const startIndex = (page - 1) * this.pageSize;
+    const endIndex = Math.min(startIndex + this.pageSize, this.users.length);
+    this.paginatedUsers = this.users.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number) {
+    this.setPage(page);
   }
 }
