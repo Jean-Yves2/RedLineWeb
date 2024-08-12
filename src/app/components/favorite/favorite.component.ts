e085cd4973da47cc4449804005c8f567f4c90a7f
Jean-Yves2 <sarakajean1@gmail.com>
2024-08-12 23:16:49 +0200

    feat : get image on product Card

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -12,7 +12,7 @@ import { Observable } from 'rxjs';
 export class FavoriteComponent implements OnInit {
   favorites: any[] = [];
   favoriteProducts: Produit[] = [];
-  produits: { [key: string]: Produit[] } = {};
+  produits: { [key: string]: { id: string, details?: string, range?: { start: number, end: number }, nom: string, schema: string, image: string }[] } = {};
   havedFavorites: boolean = false;
 
   constructor(
@@ -30,14 +30,37 @@ export class FavoriteComponent implements OnInit {
       favorites.subscribe((data) => {
         this.favorites = data;
         console.log(this.favorites);
+        this.updateFavoritesWithImages();
       });
     } else {
       this.favorites = favorites;
+      this.updateFavoritesWithImages();
     }
   }
 
+  findProductByCode(productCode: number): { schema: string, image: string } | null {
+    for (const key in this.produits) {
+      if (this.produits.hasOwnProperty(key)) {
+        for (const product of this.produits[key]) {
+          if (product.range && productCode >= product.range.start && productCode <= product.range.end) {
+            return { schema: product.schema, image: product.image };
+          }
+        }
+      }
+    }
+    return null;
+  }
 
-
+  updateFavoritesWithImages(): void {
+    this.favorites = this.favorites.map((favorite) => {
+      const productImageData = this.findProductByCode(favorite.productCode);
+      return {
+        ...favorite,
+        schema: productImageData?.schema || 'assets/images/default-schema.png',
+        image: productImageData?.image || 'assets/images/default-image.png',
+      };
+    });
+  }
 
   removeFavorite(item: any): void {
     this.favorieService.removeFavorite(item.fav_id);
