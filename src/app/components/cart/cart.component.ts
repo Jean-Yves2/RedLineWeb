ebc50477fc6839ba4f7f6732ad9783eeb644d32f
Jean-Yves2 <sarakajean1@gmail.com>
2024-08-29 12:10:33 +0200

    fix : revert

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -14,7 +14,7 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
     this.loadCart();
   }
-
+  //
   loadCart(): void {
     this.cartService.getCartByUserId().subscribe((data) => {
       this.cart = data;
