3aa435768081055180fa5d6835568022d49ef73b
Jean-Yves2 <sarakajean1@gmail.com>
2024-09-08 23:43:13 +0200

    fix: prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-metal-factory-9b52aa3c8e87.herokuapp.com/',
+  apiUrl: 'https://api-metal-factory-9b52aa3c8e87.herokuapp.com',
 };
